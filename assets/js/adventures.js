94d26840558be0ad6fa2f4f709e1d7767a1fe23d
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-06-21 13:52:02 +0300

    bag fixed

--- assets/js/adventures.js
+++ assets/js/adventures.js
@@ -19,18 +19,6 @@ loadMoreBtn.addEventListener("click", function () {
 
 // =================== Load More End ===================================
 
-// ====================== Help Center Start =======================
-
-let helpBtn = document.getElementById("help-btn");
-let helpCenter = document.getElementById("help-center");
-let helpCenterForm = document.querySelector(".help-center-form");
-
-helpBtn.addEventListener("click", function () {
-  helpCenter.classList.toggle("show");
-  helpCenterForm.classList.toggle("show-top");
-});
-
-// ====================== Help Center End =======================
 
 // ======================== Show Elements Start =======================
 
--- assets/js/destination.js
+++ assets/js/destination.js
@@ -28,26 +28,15 @@ let swiper = new Swiper(".slider-1", {
   on: {
     slideChange: function () {
       const activeSlide = this.slides[this.activeIndex];
-      const imageUrl = activeSlide.querySelector("img").src;
-      document.querySelector(".home").style.backgroundImage = `url(${imageUrl})`;
+      if (activeSlide) {
+        const imageUrl = activeSlide.querySelector("img").src;
+        document.querySelector(".home").style.backgroundImage = `url(${imageUrl})`;
+      }
     },
   },
 });
 
 /*==================== First Slider End  ====================*/
-
-// ====================== Help Center Start =======================
-
-let helpBtn = document.getElementById("help-btn");
-let helpCenter = document.getElementById("help-center");
-let helpCenterForm = document.querySelector(".help-center-form");
-
-helpBtn.addEventListener("click", function () {
-  helpCenter.classList.toggle("show");
-  helpCenterForm.classList.toggle("show-top");
-});
-
-// ====================== Help Center End =======================
 
 /*==================== Second Swiper On Landing Page  ====================*/
 
