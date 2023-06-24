f3053dcfeac3916012377f7e595cd83e4753a834
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-06-24 18:07:44 +0300

    new page

--- assets/js/work.js
+++ assets/js/work.js
@@ -11,3 +11,17 @@ helpBtn.addEventListener("click", function () {
 
 // ====================== Help Center End =======================
 
+// Ініціалізуємо ScrollReveal
+ScrollReveal().reveal('.landing-content', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.title-section', { delay: 400, distance: '50px', origin: 'left', duration: 1000 });
+ScrollReveal().reveal('.your-trip-text', { delay: 700, distance: '50px', origin: 'left', duration: 1000 });
+ScrollReveal().reveal('.daily', { delay: 400, distance: '50px', origin: 'left', duration: 1000 });
+ScrollReveal().reveal('.number', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-fs', { delay: 500, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-sc', { delay: 500, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-fs-images', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-sc-images', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-fs-content', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-sc-content', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
+
+// Інші налаштування ScrollReveal можна додати тут
