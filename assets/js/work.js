37ad755e0387ffc54eebb0c1c5ac3fd6e41521cc
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-06-25 17:28:52 +0300

    new page

--- assets/js/work.js
+++ assets/js/work.js
@@ -36,9 +36,9 @@ helpBtn.addEventListener("click", function () {
 // ====================== Help Center End =======================
 
 // Ініціалізуємо ScrollReveal
-ScrollReveal().reveal('.landing-content', { delay: 200, distance: '50px', origin: 'right', duration: 1000 });
-ScrollReveal().reveal('.title-section', { delay: 400, distance: '50px', origin: 'left', duration: 1000 });
-ScrollReveal().reveal('.your-trip-text', { delay: 700, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.landing-content', { delay: 400, distance: '50px', origin: 'right', duration: 1000 });
+ScrollReveal().reveal('.title-section', { delay: 700, distance: '50px', origin: 'left', duration: 1000 });
+ScrollReveal().reveal('.your-trip-text', { delay: 1200, distance: '50px', origin: 'bottom', duration: 1000 });
 ScrollReveal().reveal('.daily', { delay: 400, distance: '50px', origin: 'left', duration: 1000 });
 ScrollReveal().reveal('.number', { delay: 400, distance: '50px', origin: 'right', duration: 1000 });
 ScrollReveal().reveal('.daily-fs', { delay: 500, distance: '50px', origin: 'left', duration: 1000 });
