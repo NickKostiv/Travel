49ace0367ae828538863c6c3630fe9c28e413b34
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-06-25 13:45:56 +0300

    new page

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var readMoreBtns = document.querySelectorAll('.read-more-btn');
+  
+  readMoreBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      var contentBlock = btn.parentNode.querySelector('.daily-fs-content-text, .daily-sc-content-text');
+      contentBlock.classList.toggle('active');
+      
+      if (contentBlock.classList.contains('active')) {
+        btn.textContent = 'Show less';
+      } else {
+        btn.textContent = 'Read more';
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
 // ====================== Help Center Start =======================
 
 let helpBtn = document.getElementById("help-btn");
@@ -18,10 +42,18 @@ ScrollReveal().reveal('.your-trip-text', { delay: 700, distance: '50px', origin:
 ScrollReveal().reveal('.daily', { delay: 400, distance: '50px', origin: 'left', duration: 1000 });
 ScrollReveal().reveal('.number', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
 ScrollReveal().reveal('.daily-fs', { delay: 500, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.experience-title', { delay: 100, distance: '50px', origin: 'bottom', duration: 1000 });
 ScrollReveal().reveal('.daily-sc', { delay: 500, distance: '50px', origin: 'bottom', duration: 1000 });
-ScrollReveal().reveal('.daily-fs-images', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-fs-images', { delay: 300, distance: '50px', origin: 'bottom', duration: 1000 });
 ScrollReveal().reveal('.daily-sc-images', { delay: 200, distance: '50px', origin: 'bottom', duration: 1000 });
-ScrollReveal().reveal('.daily-fs-content', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
+ScrollReveal().reveal('.daily-fs-content', { delay: 700, distance: '50px', origin: 'bottom', duration: 1000 });
 ScrollReveal().reveal('.daily-sc-content', { delay: 400, distance: '50px', origin: 'bottom', duration: 1000 });
 
 // Інші налаштування ScrollReveal можна додати тут
+
+
+
+
+
+
+
