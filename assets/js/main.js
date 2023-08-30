54556cbc2c8d293d8b0991a0bca804513cb67919
Nikolas Kostiv <nikolaykostiv@gmail.com>
2023-08-30 15:48:45 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,23 +17,4 @@ navbarImg.addEventListener("click", function () {
   navbarImg.classList.toggle("active");
 });
 
-// ====================== Help Center Start =======================
-
-let helpBtn = document.getElementById("help-btn");
-let closeBtn = document.getElementById("close"); // The close button element
-let helpCenter = document.getElementById("help-center");
-let helpCenterForm = document.querySelector(".help-center-form");
-
-helpBtn.addEventListener("click", function () {
-  helpCenter.classList.toggle("show");
-  helpCenterForm.classList.toggle("show-top");
-});
-
-closeBtn.addEventListener("click", function () {
-  helpCenter.classList.remove("show"); // Hide the help center
-  helpCenterForm.classList.remove("show-top"); // Slide the form back down
-});
-
-// ====================== Help Center End =======================
-
 //===================== Navbar Close End ==========================
